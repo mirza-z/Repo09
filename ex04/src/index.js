05449ae2463190df328309e601cb0c05ec162e9d
mirza-z <zaimovic330@gmail.com>
2022-04-05 15:14:27 +0200

    Added ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function compareDifferentValues(m, n){
+    if(m === n){
+        return "Equal";
+    }
+    return "Not equal";
+}
+compareDifferentValues(8, "8");
+compareDifferentValues("8", 8);
+compareDifferentValues('8', 8);
+compareDifferentValues("8", "8");
+compareDifferentValues(8, 8);
+// Only change code above this line
+module.exports = compareDifferentValues;
