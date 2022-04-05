61ff137467985360700f8318216949e707f2cdeb
mirza-z <zaimovic330@gmail.com>
2022-04-05 15:14:35 +0200

    Added ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function compareStrictValues(m){
+    if(m != 55){
+        return "Not equal";
+    }
+    return "Equal";
+}
+compareStrictValues(55);
+compareStrictValues("55");
+compareStrictValues("21");
+compareStrictValues(12);
+compareStrictValues("Doe");
+// Only change code above this line
+module.exports = compareStrictValues;
