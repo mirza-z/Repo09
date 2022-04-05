dc4a4ba7ff0d242d2cd73873a1404dd33a81be4c
mirza-z <zaimovic330@gmail.com>
2022-04-05 15:48:09 +0200

    Added exercises from ex06 to ex11

---
+++ ex08/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function logicalAndOperator(num){
+    if(num <= 80 && num >= 40){
+        return "Yes";
+    }
+    return "No";
+}
+logicalAndOperator(0);
+logicalAndOperator(24);
+logicalAndOperator(45);
+logicalAndOperator(57);
+logicalAndOperator(100);
+// Only change code above this line
+module.exports = logicalAndOperator;
---
+++ ex09/src/index.js
@@ -0,0 +1,16 @@
+// Only change code below this line
+function logicalOrOperator(num){
+    if(num > 30 || num < 20){
+        return "Out";
+    }
+    return "In";
+}
+logicalOrOperator(0);
+logicalOrOperator(9);
+logicalOrOperator(20);
+logicalOrOperator(23);
+logicalOrOperator(30);
+logicalOrOperator(31);
+logicalOrOperator(105);
+// Only change code above this line
+module.exports = logicalOrOperator;
