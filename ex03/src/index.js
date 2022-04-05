4ffc61437b39146a22c5bd48b28089dd3473fc0b
mirza-z <zaimovic330@gmail.com>
2022-04-05 15:14:18 +0200

    Added ex03

---
+++ ex03/src/index.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function myEqualFunction(num){
+    if(num === 23){
+        return "Equal";
+    }
+    return "Not equal";
+}
+myEqualFunction(5);
+myEqualFunction(23);
+myEqualFunction("23");
+myEqualFunction('23');
+myEqualFunction("text");
+// Only change code above this line
+module.exports = myEqualFunction;
