6fb944bf789ba0661375b763d9c0d9eaec4898ca
mirza-z <zaimovic330@gmail.com>
2022-04-06 01:40:04 +0200

    Made some aditional changes

--- ex16/src/index.js
+++ ex16/src/index.js
@@ -1,20 +1,21 @@
 function switchCaseSequential(num){
 // Write code below this line
+    var answer;
     switch(num){
         case 1:
         case 2:
         case 3: 
-            return "Low";
+            answer = "Low";
             break;
         case 4:
         case 5:
         case 6: 
-            return "Mid";
+            answer =  "Mid";
             break;
         case 7:
         case 8:
         case 9:
-            return "High";
+            answer = "High";
             break;
     }
 // Write code above this line
